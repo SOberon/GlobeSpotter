870d5e17d7bdd0596cce174fdd599fed7ab95f30
Sean O'Brien <msobrien90@gmail.com>
2018-09-17 14:29:43 -0600

    Added unittest import, changed "GlobeSpotter" method name to "main".

--- GlobeSpotter.py
+++ GlobeSpotter.py
@@ -1,4 +1,24 @@
-class GlobeSpotter:
-    class __main__:
-        def __init__(self):
-            pass
+import unittest
+
+class main:
+
+    def main(self):
+        pass
+
+    def file_reader(self):
+        import re
+
+        file = open("temp.log")
+        ipList = []
+
+        for line in file:
+            ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
+            ipList.append(ip)
+
+
+class UnitTests(unittest.TestCase):
+
+    def empty_test(self):
+        pass
+
+    if __name__ == "__main__": main()
