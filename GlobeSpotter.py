7f6e67a31c1162dc1f7fbb28acf747fcf0d7a3a8
Sean O'Brien <msobrien90@gmail.com>
2018-09-19 15:08:12 -0600

    Abandoned the regex approach for checking IP addresses, and instead just check if each token is a valid address using the socket library.

--- GlobeSpotter.py
+++ GlobeSpotter.py
@@ -1,11 +1,8 @@
 import unittest
+import re
+import socket
+import tokenize
 import pydoc
-import IPaddresses
-import Data
-
-from Data import CleanData  # Format and clean the data
-from IPAddresses import ExtractIPs
-
 
 
 class main:
@@ -13,9 +10,9 @@ class main:
     def main(self):
         pass
 
-    def get_ipv4s_from_file(input_file):
+    def valid_ip(input_file):
         """
-        https://stackoverflow.com/questions/14026529/python-parse-file-for-ip-addresses
+        https://stackoverflow.com/questions/11264005/using-a-regex-to-match-ip-addresses-in-python/11264056
 
         Parse a .txt file for IPv4 and IPv6 addresses.
 
@@ -27,27 +24,33 @@ class main:
 
         ip_list = []
 
-        ip_list.extend(CleanData(input_file).to_list())
+        tokens = tokenize.open(input_file)
 
-        return ExtractIPs(ip_list).get_ipv4_results()
+        for ip in tokens:
 
-    def get_ipv6s_from_file(input_file):
-        """
-        https://stackoverflow.com/questions/14026529/python-parse-file-for-ip-addresses
+            if socket.inet_aton(ip):
+                ip_list.append(ip)
 
-        Parse a .txt file for IPv4 and IPv6 addresses.
+        return ip_list
 
-        Args:
-            input_file: A .txt file to be parsed.
 
-        :return: A list of IP addresses.
-        """
-
-        ip_list = []
-
-        ip_list.extend(CleanData(input_file).to_list())
-
-        return ExtractIPs(ip_list).get_ipv4_results()
+    # def get_ipv6s_from_file(input_file):
+    #     """
+    #     https://stackoverflow.com/questions/14026529/python-parse-file-for-ip-addresses
+    #
+    #     Parse a .txt file for IPv4 and IPv6 addresses.
+    #
+    #     Args:
+    #         input_file: A .txt file to be parsed.
+    #
+    #     :return: A list of IP addresses.
+    #     """
+    #
+    #     ip_list = []
+    #
+    #     ip_list.extend(CleanData(input_file).to_list())
+    #
+    #     return ExtractIPs(ip_list).get_ipv4_results()
 
 class UnitTests(unittest.TestCase):
 
